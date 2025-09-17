d0b48c2c9da41dc3c91c146b4a23eae9b1ab0a62
Maite <maitesolam@gmail.com>
2025-09-17 11:09:55 +0200

    Primer ejercicio con llamada a variables

--- PrimerEjercicio/index.php
+++ PrimerEjercicio/index.php
@@ -2,11 +2,28 @@
 $nombre="Maite";
 $edad= "33";
 
+$mensajeEdad="h";
+if ($edad > 18){
+    $mensajeEdad="Eres mayor de edad";
+} else {
+    $mensajeEdad= "Eres menor de edad";
+}
+
+$numeros="";
+for ($i=0; $i<=10; $i++){
+    $numeros= $numeros .$i." ";
+
+}
+
+
 ?>
 <html>
 
 <h1>Bienvenido a mi sitio web PHP </h1>
 <p>Hola <?php echo $nombre ?> tienes <?php echo $edad ?></p>
 
+<?php 
+echo $mensajeEdad;
+echo $numeros
+?>
 
-</html>
