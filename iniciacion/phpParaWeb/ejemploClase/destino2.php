273d1861fb8b6d78ace59e2960658f2f56d12650
Maite <maitesolam@gmail.com>
2025-09-26 10:33:19 +0200

    Ejemplos de php para web

---
+++ iniciacion/phpParaWeb/ejemploClase/destino2.php
@@ -0,0 +1,8 @@
+<?php 
+
+echo "Nombre: " . $_POST['nombre'] . "<br>\n";
+echo "Apellidos: " . $_POST['apellidos'] . "<br>\n";
+
+//Si lo declaras antes puedes ponerle así: echo "Nombre: " . htmlspecialchars($nombre) . "<br>\n";
+
+?>
---
+++ iniciacion/phpParaWeb/ejemploClase/destinoGet.php
@@ -0,0 +1,7 @@
+<?php 
+$saludo= $_GET["saludo"];
+$texto = $_GET["texto"];
+
+echo "Variable $saludo <br>\n";
+echo "Variable $texto <br>\n";
+?>
